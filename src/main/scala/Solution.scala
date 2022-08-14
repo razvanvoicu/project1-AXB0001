51642e0a67ccd785bf9b13d3bd954c92e62c0fea
Syed Mustafa Umar <mustafa@Syeds-MacBook-Pro.local>
2022-08-15 03:12:47 +0800

    Final submission v1.0

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -1,2 +1,341 @@
+import scala.annotation.targetName
+import scala.collection.immutable.Queue
+import Math.{floor, max, min, log10}
+
+// Need to change height as well to max(__1.height, __2.height, ...)
+
+sealed trait IArr[+T]:
+  def height: Int           // distance from current node to any leaf
+  def length: Int           // size of the array
+  def isEmpty: Boolean
+  def apply(i: Int): T      // array dereference, 0-based. a(3) returns element at index 3.
+  def update[T1 >: T](i: Int, value: T1): IArr[T1] // return a new array which is different only at index i
+  def iterator: Iterator[T] // iterate through all the elements of an array
+
+  def slice(idx1: Int, idx2: Int): IArr[T] = // returns the slice between min(idx1, idx2) and max(idx1, idx2)
+    IArr(iterator.slice(idx1, idx2).toSeq : _*)
+
+  // Convenience methods, to be used in the rest of the classes
+  def outOfBounds(i: Int): Nothing =
+    throw new IndexOutOfBoundsException(s"Attempt to access non-existent index $i in an array of size $length")
+
+  def withinBounds[T1](i: Int)(code: => T1): T1 =
+    if i < 0 || i >= length then outOfBounds(i)
+    code
+
+  def withinBoundsF[T1](i: Int)(f: Int => T1): T1 = withinBounds(i)(f(i))
+
+  def append[T1 >: T](secArr: IArr[T1]): IArr[T1]
+
+  def log4(x: Double): Int = floor(log10(x) / log10(4.0)).toInt
+
+  def condForAppend: Boolean = 2 * log4(length) < height
+
+object IArr: // Companion object, containing the constructor
+  def  apply[T](elems: T*): IArr[T] = // Construct an array from a list of arguments
+    val leafs = elems.grouped(4).map {
+      case Seq() => IArrLeaf0
+      case Seq(a1) => IArrLeaf1(a1)
+      case Seq(a1, a2) => IArrLeaf2(a1, a2)
+      case Seq(a1, a2, a3) => IArrLeaf3(a1, a2, a3)
+      case Seq(a1, a2, a3, a4) => IArrLeaf4(a1, a2, a3, a4)
+      case _ => assert(false)
+    }.to(List)
+    Iterator.iterate((leafs: List[IArr[T]], leafs.length)) {
+      case (l, count) =>
+        val nextLevel = l.grouped(4).map {
+          case Seq(a1) => IArrNode1(a1)
+          case Seq(a1, a2) => IArrNode2(a1, a2)
+          case Seq(a1, a2, a3) => IArrNode3(a1, a2, a3)
+          case Seq(a1, a2, a3, a4) => IArrNode4(a1, a2, a3, a4)
+          case _ => assert(false)
+        }.to(List)
+        (nextLevel, (count+3)/4)
+    }.dropWhile(_._2 > 1).next._1.headOption.getOrElse(IArrLeaf0)
+
+
+
+sealed trait IArrLeaf[+T] extends IArr[T]: // Array leafs, coming in 5 sizes: empty, with 1 elem, with 2 elems, with 3 elems, and with 4 elems
+  override def height = 0
+
+case object IArrLeaf0 extends IArrLeaf[Nothing] { // Leaf of size 0, usable for empty arrays
+  override def length: Int = 0
+  override def isEmpty: Boolean = true
+  override def apply(i: Int): Nothing = outOfBounds(i)
+  override def update[T1 >: Nothing](i: Int, value: T1): IArr[T1] = outOfBounds(i)
+  override def iterator: Iterator[Nothing] = Iterator.empty
+  override def slice(idx1: Int, idx2: Int): IArr[Nothing] = outOfBounds(idx1)
+  override def append[T1 >: Nothing](secArr: IArr[T1]): IArr[T1] = secArr
+}
+
+
+sealed trait IArrLeafNonEmpty[+T] extends IArrLeaf[T]: // Base trait for the 4 non-empty leafs
+  override def isEmpty: Boolean = false
+
+case class IArrLeaf1[+T](__1: T) extends IArrLeafNonEmpty[T]: // Leaf with 1 elem
+  override def apply(i: Int): T = withinBounds(i)(__1)
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBounds(i)(IArrLeaf1(value))
+  override def iterator: Iterator[T] = Iterator(__1)
+  override def length: Int = 1
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(this, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq : _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+  }
+
+
+case class IArrLeaf2[+T](__1: T, __2: T) extends IArrLeafNonEmpty[T]: // Leaf with 2 elems
+  override def length: Int = 2
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i % 2 == 0 => __1
+    case _ if i % 2 == 1 => __2
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] =
+    withinBoundsF(i) {
+    case _ if i % 2 == 0 => IArrLeaf2(value, __2)
+    case _ if i % 2 == 1 => IArrLeaf2(__1, value)
+  }
+
+  override def iterator: Iterator[T] = Iterator(__1, __2)
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(this, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+  }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+
+case class IArrLeaf3[+T](__1: T, __2: T, __3: T) extends IArrLeafNonEmpty[T]: // Leaf with 3 elems
+  override def length: Int = 3
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i % 3 == 0 => __1
+    case _ if i % 3 == 1 => __2
+    case _ if i % 3 == 2 => __3
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBoundsF(i) {
+    case _ if i % 3 == 0 => IArrLeaf3(value, __2, __3)
+    case _ if i % 3 == 1 => IArrLeaf3(__1, value, __3)
+    case _ if i % 3 == 2 => IArrLeaf3(__1, __2, value)
+  }
+
+  override def iterator: Iterator[T] = Iterator(__1, __2, __3)
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(this, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+  }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+
+case class IArrLeaf4[+T](__1: T, __2: T, __3: T, __4: T) extends IArrLeafNonEmpty[T]: // Leafs with 4 elems
+  override def length: Int = 4
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i % 4 == 0 => __1
+    case _ if i % 4 == 1 => __2
+    case _ if i % 4 == 2 => __3
+    case _ if i % 4 == 3 => __4
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBoundsF(i) {
+    case _ if i % 4 == 0 => IArrLeaf4(value, __2, __3, __4)
+    case _ if i % 4 == 1 => IArrLeaf4(__1, value, __3, __4)
+    case _ if i % 4 == 2 => IArrLeaf4(__1, __2, value, __4)
+    case _ if i % 4 == 3 => IArrLeaf4(__1, __2, __3, value)
+  }
+
+  override def iterator: Iterator[T] = Iterator(__1, __2, __3, __4)
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(this, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+  }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+
+
+
+sealed trait IArrNode[+T] extends IArr[T]: // Base trait for all inner nodes, coming in 4 sizes
+  // def __1: IArr[T]
+
+  // override val height: Int = __1.height + 1
+
+  override def isEmpty: Boolean = false
+
+  // Convenience methods to be used in all the nodes
+  def withinBoundsWithIndexSplit[T](i: Int)(f: ((Int, Int)) => T): T =
+    withinBounds(i)(f(indexSplit(i)))
+
+  def indexSplit(i: Int): (Int, Int) =
+    val cellCapacity = 1 << (2 * height)
+    (i / cellCapacity, i % cellCapacity)
+
+
+case class IArrNode1[+T](__1: IArr[T]) extends IArrNode[T]:
+  override val length: Int = __1.length
+
+  override val height: Int = __1.height + 1
+
+  override def apply(i: Int): T = withinBoundsF(i)(_ => __1(i))
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] =
+    withinBoundsF(i)(_ => IArrNode1(__1.update(i, value)))
+
+  override def iterator: Iterator[T] = __1.iterator
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(__1, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+  }
+    // if (!secArr.isEmpty) IArrNode2(__1, secArr) else this
+
+
+case class IArrNode2[+T](__1: IArr[T], __2: IArr[T]) extends IArrNode[T]:
+  override val length: Int = __1.length + __2.length
+
+  override val height: Int = max(__1.height, __2.height) + 1
+
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i < __1.length => __1(i)
+    case _ => __2(i - __1.length)
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBoundsF(i) {
+    case _ if i < __1.length => IArrNode2(__1.update(i, value), __2)
+    case _ => IArrNode2(__1, __2.update(i - __1.length, value))
+  }
+
+  override def iterator: Iterator[T] = __1.iterator ++ __2.iterator
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode3(__1, __2, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+  }
+    // if (!secArr.isEmpty) IArrNode3(__1, __2, secArr) else this
+
+
+case class IArrNode3[+T](__1: IArr[T], __2: IArr[T], __3: IArr[T]) extends IArrNode[T]:
+  override val length: Int = __1.length + __2.length + __3.length
+
+  override val height: Int = max(__1.height, max(__2.height, __3.height)) + 1
+
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i < __1.length => __1(i)
+    case _ if i < __1.length + __2.length => __2(i - __1.length)
+    case _ => __3(i - (__1.length + __2.length))
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBoundsF(i) {
+    case _ if i < __1.length => IArrNode3(__1.update(i, value), __2, __3)
+    case _ if i < __1.length + __2.length => IArrNode3(__1, __2.update(i - __1.length, value), __3)
+    case _ => IArrNode3(__1, __2, __3.update(i - (__1.length + __2.length), value))
+  }
+
+  override def iterator: Iterator[T] = __1.iterator ++ __2.iterator ++ _3.iterator
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode4(__1, __2, __3, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+  }
+    // if (!secArr.isEmpty) IArrNode4(__1, __2, __3, secArr) else this
+
+
+case class IArrNode4[+T](__1: IArr[T], __2: IArr[T], __3: IArr[T], __4: IArr[T]) extends IArrNode[T]:
+  override val length: Int = __1.length + __2.length + __3.length + __4.length
+
+  override val height: Int = max(__1.height, max(__2.height, max(__3.height, __4.height))) + 1
+
+  override def apply(i: Int): T = withinBoundsF(i) {
+    case _ if i < __1.length => __1(i)
+    case _ if i < __1.length + __2.length => __2(i - __1.length)
+    case _ if i < __1.length + __2.length + __3.length => __3(i - (__1.length + __2.length))
+    case _ => __4(i - (__1.length + __2.length + __3.length))
+  }
+
+  override def update[T1 >: T](i: Int, value: T1): IArr[T1] = withinBoundsF(i) {
+    case _ if i < __1.length =>
+      IArrNode4(__1.update(i, value), __2, __3, __4)
+    case _ if i < __1.length + __2.length =>
+      IArrNode4(__1, __2.update(i - __1.length, value), __3, __4)
+    case _ if i < __1.length + __2.length + __3.length =>
+      IArrNode4(__1, __2, __3.update(i - (__1.length + __2.length), value), __4)
+    case _ =>
+      IArrNode4(__1, __2, __3, __4.update(i - (__1.length + __2.length + __3.length), value))
+  }
+
+  override def iterator: Iterator[T] = __1.iterator ++ __2.iterator ++ _3.iterator ++ _4.iterator
+
+  override def append[T1 >: T](secArr: IArr[T1]): IArr[T1] = {
+    if (!secArr.isEmpty) {
+      val newNode = IArrNode2(this, secArr)
+      if (newNode.condForAppend) {
+        IArr(newNode.iterator.toSeq: _*)
+      } else {
+        newNode
+      }
+    } else {
+      this
+    }
+  }
+    // if (!secArr.isEmpty) IArrNode2(this, secArr) else this
+
+
 
 object Solution // Keep IntelliJ happy
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -2,11 +2,204 @@ import org.scalacheck.*
 import org.scalacheck.Prop.*
 import org.scalacheck.Test.*
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.Assertions.*
+import org.scalatest.matchers.should.Matchers._
 
 import scala.util.Random
 
 class Test extends AnyFunSuite:
+
+  test("The array constructor works correctly") {
+    IArr() shouldBe IArrLeaf0
+    IArr(1) shouldBe IArrLeaf1(1)
+    IArr(1,2) shouldBe IArrLeaf2(1,2)
+    IArr(1,2,3) shouldBe IArrLeaf3(1,2,3)
+    IArr(1,2,3,4) shouldBe IArrLeaf4(1,2,3,4)
+    IArr(1,2,3,4,5) shouldBe IArrNode2(IArrLeaf4(1,2,3,4), IArrLeaf1(5))
+    IArr(1 to 16 : _*) shouldBe IArrNode4(
+      IArrLeaf4(1,2,3,4), IArrLeaf4(5,6,7,8), IArrLeaf4(9,10,11,12), IArrLeaf4(13,14,15,16)
+    )
+    IArr((1 to 17): _*) shouldBe IArrNode2(
+      IArrNode4(IArrLeaf4(1,2,3,4), IArrLeaf4(5,6,7,8), IArrLeaf4(9,10,11,12), IArrLeaf4(13,14,15,16)),
+      IArrNode1(IArrLeaf1(17))
+    )
+  }
+
+  test("isEmpty works correctly") {
+    IArr().isEmpty shouldBe true
+    IArr(1,2,3,4,5).isEmpty shouldBe false
+    IArr().append(IArr()).isEmpty shouldBe true
+    IArr(1).append(IArr()).isEmpty shouldBe false
+    IArr().append(IArr(100)).isEmpty shouldBe false
+  }
+
+  test("append works correctly (simple)") {
+    // println(IArr().append(IArr()))
+    // println(IArr(1).append(IArr()))
+    // println(IArr(1,2,3,4,5).append(IArr(1)).append(IArr(1,2,3,4,5)).append(IArr(1,2,3,4,5)))
+    // println(IArr(1,2,3,4,5).append(IArr(1)).append(IArr(1,2,3,4,5)).append(IArr(1,2,3,4,5)).length)
+    // IArr(1,2,3,4,5).append(IArr(6)).append(IArr(7,8,9,10,11)).append(IArr(12,13,14,15,16)).iterator.foreach(println)
+    // IArr((1 to 17): _*).append(IArr((1 to 170): _*)).append(IArr((500 to 600): _*)).iterator.foreach(println)
+    IArr(1,2,3,4,5).append(IArr(6,7,8,9)) shouldBe
+      IArrNode3(IArrLeaf4(1,2,3,4),IArrLeaf1(5),IArrLeaf4(6,7,8,9))
+    IArr(1,2).append(IArr(3,4)) shouldBe IArrNode2(IArrLeaf2(1,2),IArrLeaf2(3,4))
+    IArr().append(IArr(1)) shouldBe IArrLeaf1(1)
+    IArr().append(IArr()) shouldBe IArrLeaf0
+    IArr(1,2,3,4).append(IArr(5,6,7,8)).append(IArr(9,10,11,12)).append(IArr(13,14,15,16)) shouldBe
+      IArr(1 to 16 : _*)
+
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.listOfN(10, Gen.choose(1, 10)))
+      forAll(lg) { (l: List[List[Int]]) =>
+        var arr = IArr(l.head: _*)
+        for (i <- 1 until l.length) {
+          arr = arr.append(IArr(l(i) : _*))
+        }
+        arr.iterator.toList == l.flatten
+      }
+    }(_.withMinSuccessfulTests(200)).status shouldBe Passed
+  }
+
+  // Change the condForAppend function in the IArr trait
+  // to see the results of different conditions for append.
+  test("append works correctly (stress)") {
+
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.listOfN(10000, Gen.choose(-100, 100)))
+      forAll(lg) { (l: List[List[Int]]) =>
+        var arr = IArr(l.head: _*)
+        for (i <- 1 until l.length) {
+          arr = arr.append(IArr(l(i): _*))
+        }
+        val dummy = arr(arr.length - 1)
+        val dummyToo = arr((arr.length - 1) / 2)
+        arr.iterator.toList == l.flatten
+      }
+    }(_.withMinSuccessfulTests(50)).status shouldBe Passed
+
+  }
+
+  test("length works correctly") {
+    IArr().length shouldBe 0
+    IArr(1).length shouldBe 1
+    IArr(1,2).length shouldBe 2
+    IArr(1,2,3).length shouldBe 3
+    IArr(1,2,3,4).length shouldBe 4
+    IArr(1,2,3,4,5).length shouldBe 5
+    IArr(1 to 17 : _*).length shouldBe 17
+
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.stringOf(Gen.choose(' ', 'z')))
+      forAll(lg) { (l: List[String]) =>
+        val rept = Random.nextInt(10)
+        val lists = for (_ <- 0 until rept) yield l
+        val arr = lists.foldLeft(IArr(""))((acc, ls) => acc.append(IArr(ls: _*)))
+        arr.length == l.length * rept + 1
+      }
+    }(_.withMinSuccessfulTests(100)).status shouldBe Passed
+  }
+
+  test("height works correctly") {
+    IArr().height shouldBe 0
+    IArr(1).height shouldBe 0
+    IArr(1,2).height shouldBe 0
+    IArr(1,2,3).height shouldBe 0
+    IArr(1,2,3,4).height shouldBe 0
+    IArr(1,2,3,4,5).height shouldBe 1
+    IArr(1 to 16 : _*).height shouldBe 1
+    IArr(1 to 17 : _*).height shouldBe 2
+    IArr(1 to 65 : _*).height shouldBe 3
+    IArr(1 to ((1 << 10)+1): _*).height shouldBe 5
+    IArr(1 to 16 : _*).append(IArr(1 to 16 : _*))
+      .append(IArr(1 to 16 : _*)).append(IArr(1 to 16 : _*)).height shouldBe 2
+    IArr(1).append(IArr(1 to 16: _*)).height shouldBe 2
+    IArr(1).append(IArr(1 to 17: _*)).height shouldBe 3
+    IArr(1).append(IArr(1 to 65: _*)).height shouldBe 4
+    IArr(1 to ((1 << 10)+1): _*).append(IArr(1 to ((1 << 10)+1): _*)).height shouldBe 6
+  }
+
+  test("array access works correctly") {
+    intercept[IndexOutOfBoundsException](IArr()(0))
+    IArr(1)(0) shouldBe 1
+    IArr(1,2)(1) shouldBe 2
+    IArr(1,2)(0) shouldBe 1
+    intercept[IndexOutOfBoundsException](IArr(1,2)(3))
+    IArr(1,2,3)(2) shouldBe 3
+    IArr(1,2,3,4)(3) shouldBe 4
+    IArr(1,2,3,4,5)(4) shouldBe 5
+    IArr(1 to 17 : _*)(16) shouldBe 17
+    IArr(1 to 17 : _*)(10) shouldBe 11
+    IArr(1 to 1000 : _*)(998) shouldBe 999
+    IArr(1 to 1000 : _*)(500) shouldBe 501
+    intercept[IndexOutOfBoundsException](IArrNode1(IArrLeaf4(10,20,30,40))(4))
+    IArrNode1(IArrLeaf4(10,20,30,40))(3) shouldBe 40
+
+    intercept[IndexOutOfBoundsException](IArr().append(IArr())(0))
+    IArr(1).append(IArr(1))(0) shouldBe 1
+    IArr(1,2).append(IArr(1,2))(1) shouldBe 2
+    IArr(1,2).append(IArr(1,2))(0) shouldBe 1
+    intercept[IndexOutOfBoundsException](IArr(1,2).append(IArr(3))(3))
+    IArr(1,2,3).append(IArr(4,5,6))(3) shouldBe 4
+    IArr(1,2,3,4).append(IArr(5,6,7,8))(4) shouldBe 5
+    IArr(1,2,3,4).append(IArr(5))(4) shouldBe 5
+    IArr(1 to 17 : _*).append(IArr(500 to 600 : _*))(20) shouldBe 503
+    IArr(1 to 7 : _*).append(IArr(8 to 17 : _*))(10) shouldBe 11
+    IArr(1 to 500 : _*).append(IArr(501 to 1000 : _*))(998) shouldBe 999
+    IArr(1 to 500 : _*).append(IArr(501 to 1000 : _*))(500) shouldBe 501
+    IArr(1,2).append(IArr(3,4))(2) shouldBe 3
+  }
+
+  test("update works correctly") {
+    IArr(1,2,3,4).update(0, 100) shouldBe IArrLeaf4(100,2,3,4)
+    IArr(1,2,3,4,5).update(0,100) shouldBe IArrNode2(IArrLeaf4(100,2,3,4), IArrLeaf1(5))
+    IArr(1,2,3,4,5).update(4,100) shouldBe IArrNode2(IArrLeaf4(1,2,3,4), IArrLeaf1(100))
+    IArr(1 to 17 : _*).update(0, 100) shouldBe IArrNode2(
+      IArrNode4(IArrLeaf4(100,2,3,4), IArrLeaf4(5,6,7,8), IArrLeaf4(9,10,11,12), IArrLeaf4(13,14,15,16)),
+      IArrNode1(IArrLeaf1(17))
+    )
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.stringOf(Gen.choose(' ', 'z')))
+      forAll(lg) { (l: List[String]) =>
+        val idx = Random.nextInt(l.length)
+        IArr(l: _*).update(idx, Int.MinValue.toString)(idx) == Int.MinValue.toString
+      }
+    }(_.withMinSuccessfulTests(100)).status shouldBe Passed
+  }
+
+  test("slice works correctly") {
+    //    val itero = IArrNode2(IArrLeaf4(1,2,3,4),IArrLeaf1(5)).iterator
+    //    val iteroSliced = IArrNode2(IArrLeaf4(1,2,3,4),IArrLeaf1(5)).slice(0, 4).iterator
+    //    IArrNode2(IArrLeaf4(1,2,3,4),IArrLeaf1(5)).slice(0, 4) shouldBe IArrLeaf4(1,2,3,4)
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.stringOf(Gen.choose(' ', 'z')))
+      forAllNoShrink(lg) { (l: List[String]) =>
+        val idx1 = Option(l.length).filter(_ > 1).map(lgth => Random.nextInt(lgth - 1)).getOrElse(0)
+        val idx2 = Random.nextInt(l.length - idx1) + idx1
+        IArr(l: _*).slice(idx1, idx2) == IArr(l.slice(idx1, idx2): _*)
+      }
+    }(_.withMinSuccessfulTests(100)).status shouldBe Passed
+  }
+
+  test("concatenation works correctly") {
+
+    IArr(1,2,3,4).update(0, 100) shouldBe IArrLeaf4(100,2,3,4)
+    IArr(1,2,3,4,5).update(0,100) shouldBe IArrNode2(IArrLeaf4(100,2,3,4), IArrLeaf1(5))
+    IArr(1,2,3,4,5).update(4,100) shouldBe IArrNode2(IArrLeaf4(1,2,3,4), IArrLeaf1(100))
+    IArr(1 to 17 : _*).update(0, 100) shouldBe IArrNode2(
+      IArrNode4(IArrLeaf4(100,2,3,4), IArrLeaf4(5,6,7,8), IArrLeaf4(9,10,11,12), IArrLeaf4(13,14,15,16)),
+      IArrNode1(IArrLeaf1(17))
+    )
+
+    check {
+      val lg = Gen.nonEmptyListOf(Gen.stringOf(Gen.choose(' ', 'z')))
+      forAll(lg) { (l: List[String]) =>
+        val idx = Random.nextInt(l.length)
+        IArr(l: _*).update(idx, Int.MinValue.toString)(idx) == Int.MinValue.toString
+      }
+    }(_.withMinSuccessfulTests(100)).status shouldBe Passed
+
+  }
+
   test("sample") {
     check {
       forAll { (b: Boolean) => b | !b }
